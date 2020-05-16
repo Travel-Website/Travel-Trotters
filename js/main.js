04d986b7a83d1b86f3bd586bebe54c99bd6cc76c
meera90 <meerashah_90@hotmail.com>
2020-05-16 15:19:19 +0100

    add carousel and rotating cards to top pick destinantions

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var myIndex = 0;
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    carousel();
+    video1MouseHover()
+    video2MouseHover()
+    video3MouseHover()
+    
+});
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1;}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000);    
+}
+
+
+function video1MouseHover(){
+    $("#video1").mouseenter(function(){
+        $(this).attr("src",$(this).attr("src") + "?autoplay=1");
+    });
+    
+    $("#video1").mouseleave(function(){
+        var src= $(this).attr("src");
+        var arr_str = src.split("?");
+        $(this).attr("src",arr_str[0]);
+    });
+}
+
+function video2MouseHover(){
+    $("#video2").mouseenter(function(){
+        $(this).attr("src",$(this).attr("src") + "?autoplay=1");
+    });
+    
+    $("#video2").mouseleave(function(){
+        var src= $(this).attr("src");
+        var arr_str = src.split("?");
+        $(this).attr("src",arr_str[0]);
+    });
+}
+
+function video3MouseHover(){
+    $("#video3").mouseenter(function(){
+        $(this).attr("src",$(this).attr("src") + "?autoplay=1");
+    });
+    
+    $("#video3").mouseleave(function(){
+        var src= $(this).attr("src");
+        var arr_str = src.split("?");
+        $(this).attr("src",arr_str[0]);
+    });
+}
+
+
